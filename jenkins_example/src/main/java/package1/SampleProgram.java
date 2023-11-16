c180ea08e87ed7fb7fd59ccf6fc2948d611afcb7
vismayakrishna <vismayaradhakrishnan97@gmail.com>
2023-11-16 10:47:48 +0530

    Initial Commit

---
+++ jenkins_example/src/main/java/package1/SampleProgram.java
@@ -0,0 +1,14 @@
+package package1;
+
+public class SampleProgram {
+	
+	public void printHelloWorld() {
+		System.out.println("Hello World!");
+	}
+
+	public static void main(String[] args) {
+		SampleProgram obj = new SampleProgram();
+		obj.printHelloWorld();
+	}
+
+}
